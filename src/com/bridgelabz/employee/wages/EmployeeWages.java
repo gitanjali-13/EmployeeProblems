b30ec0329b9250345481f8e0d2dcd70f4713a698
gitanjali-13 <gitaanjalide@gmail.com>
2022-09-10 21:32:26 +0530

    Added:Employee daily wages

--- src/com/bridgelabz/employee/wages/EmployeeWages.java
+++ src/com/bridgelabz/employee/wages/EmployeeWages.java
@@ -3,13 +3,20 @@ package com.bridgelabz.employee.wages;
 public class EmployeeWages {
     public static void main(String[] args) {
         System.out.println("**Welcome to EMPLOYEE WAGES PROGRAM**");
+        int salary;
         int fullTime = 1;
         int perHrsWage = 20;
         double check = Math.floor(Math.random() * 10 ) % 2;
         if ( check == fullTime ) {
             System.out.println("Employee is present.");
+            int Hrs = 8;
+            salary = perHrsWage * Hrs;
+            System.out.println("Salary of employee" +salary );
         }else {
             System.out.println("Employee is not present.");
+            int Hrs = 0;
+            salary = perHrsWage * Hrs;
+            System.out.println("Salary of employee"+salary);
         }
     }
 }
