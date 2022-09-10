190f586d548c2cf5661216a7c7fe72741c48ee2b
gitanjali-13 <gitaanjalide@gmail.com>
2022-09-10 21:58:35 +0530

    Added:Part time employee wage

--- src/com/bridgelabz/employee/wages/EmployeeWages.java
+++ src/com/bridgelabz/employee/wages/EmployeeWages.java
@@ -3,20 +3,33 @@ package com.bridgelabz.employee.wages;
 public class EmployeeWages {
     public static void main(String[] args) {
         System.out.println("**Welcome to EMPLOYEE WAGES PROGRAM**");
-        int salary;
+        int salary ;
         int fullTime = 1;
+        int partTime = 2;
         int perHrsWage = 20;
-        double check = Math.floor(Math.random() * 10 ) % 2;
-        if ( check == fullTime ) {
-            System.out.println("Employee is present.");
-            int Hrs = 8;
-            salary = perHrsWage * Hrs;
-            System.out.println("Salary of employee" +salary );
-        }else {
-            System.out.println("Employee is not present.");
-            int Hrs = 0;
-            salary = perHrsWage * Hrs;
-            System.out.println("Salary of employee"+salary);
+        int monthSalary = 0;
+        for ( int i = 0; i <= 20; i++ ) {
+
+            int check = (int) Math.floor(Math.random() * 10) % 2;
+            switch (check) {
+                case 1 :
+                    System.out.println("Employee is working in part time.");
+                    int Hrs = 8;
+                    salary = perHrsWage * Hrs;
+                    System.out.println("Salary of employee :" + salary);
+                    break;
+                case 2 :
+                    System.out.println("Employee is working in part time.");
+                    Hrs = 4;
+                    salary = perHrsWage * Hrs;
+                    System.out.println("Salary of employee :" + salary);
+                    break;
+                default:
+                    System.out.println("Employee is not present!!!");
+                    Hrs = 0;
+                    salary = perHrsWage * Hrs;
+                    System.out.println("salary for employee : " + salary);
+                }
+            }
         }
     }
-}
